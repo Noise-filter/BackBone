3ece4f7d6309219fa9f1f9bccd43e910f8a4f34e
Gurkmannen <sammario_svensson@gmail.com>
2014-03-16 19:15:25 -0700

    WINAPI main function fixed, Window is fixed

--- Code/TestProject/main.cpp
+++ Code/TestProject/main.cpp
@@ -5,20 +5,107 @@ using namespace std;
 #include <vld.h>
 #include "../BackBone/BackBone.h"
 
-int main()
+LRESULT CALLBACK			WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+int Update(float deltaTime);
+int Render();
+int Init(HWND hWnd);
+
+
+
+int Update(float deltaTime)
+{
+	BackBone::Flush();
+
+	return 0;
+}
+
+int Render()
+{
+	return 0;
+}
+
+int Init(HWND hWnd)
+{
+	if (!BackBone::Init(NULL))
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
 	if (SetDllDirectory("..\\DLL") == FALSE)
 	{
 		return -1;
 	}
 
-	if (!BackBone::Init(NULL))
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszClassName = "WindowClass";
+
+	RegisterClassEx(&wc);
+
+	RECT wr = { 0, 0, 500, 400 };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	hWnd = CreateWindowEx(NULL,
+		"WindowClass",
+		"BackBone",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	Init(hWnd);
+	MSG msg;
+
+	while (true)
 	{
-		return 1;
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		Update(1.0f);
+		Render();
+		
+
 	}
 
-
-	BackBone::Flush();
-
 	return 0;
+}
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		} break;
+
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
